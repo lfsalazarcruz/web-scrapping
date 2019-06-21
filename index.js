a82596e99a4ebbac395a6798e2ff00bc2ffc34af
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2019-06-20 22:35:32 -0400

    Added fake-useragent npm package to create fake-user agents with every request to the website.

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const request = require("request-promise");
 const cheerio = require("cheerio");
+const fakeUa = require("fake-useragent");
 
 const URL = "https://www.imdb.com/title/tt0848228/?ref_=fn_al_tt_1";
 
 (async () => {
-  const response = await request(URL);
+  const response = await request({
+    uri: URL,
+    headers: {
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+      "accept-encoding": "gzip, deflate, br",
+      "accept-language": "en-US,en;q=0.9",
+      "cache-control": "max-age=0",
+      dnt: "1",
+      "upgrade-insecure-requests": "1",
+      "user-agent": fakeUa()
+    },
+    gzip: true
+  });
 
   let $ = cheerio.load(response);
 
